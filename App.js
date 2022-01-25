befa02efec31aaac12cb23c3941f61ac80463428
Evgenii <geniya59@gmail.com>
2022-01-25 13:54:23 +0300

    add

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { MainLayout } from './src/MainLayout'
 import AppLoading from 'expo-app-loading'
 import { Provider } from 'react-redux'
 import store from './src/store'
-import { init } from './src/init';
-import { THEME } from './src/theme'
+import { init } from './src/init'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
